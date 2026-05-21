6a38904ad6202f67b1d44a2447eab48b34d66c52
Flebory <123499686+Flebory@users.noreply.github.com>
2026-05-20 23:28:44 -0400

    Initialized Repository

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-400 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-white mb-4">Welcome to Vite + React + Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App;
