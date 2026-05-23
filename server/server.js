ea0c5c277409382cb75117a369c494484f370176
Flebory <123499686+Flebory@users.noreply.github.com>
2026-05-23 11:34:53 -0400

    Set up backend folder structure and boiler plate code | Set up frontend folder structure

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
+
+const userAuthRoutes = require('./routes/user-auth-routes');
+const roomManagementRoutes = require('./routes/room-management-routes');
+
+app.use('/api/user', userAuthRoutes);
+app.use('/api/room', roomManagementRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running' });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
